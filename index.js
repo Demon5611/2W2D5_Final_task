9d7027798674047983aecd312fc809568d343036
Dmitry <d555601@yandex.ru>
2023-04-15 15:04:40 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,37 +1,52 @@
-const btnAdd = document.querySelector(".button");
-btnAdd.style.backgroundColor = "red";
-btnAdd.innerText = "wert";
-button.appendChild(btnAdd);
+// const btnAdd = document.querySelector(".button");
+// btnAdd.style.backgroundColor = "red";
+// btnAdd.innerText = "wertsdfthygjhkijl";
+// button.appendChild(btnAdd);
 
-const input = document.querySelector(".input-form");
-const btn = document.querySelector(".button");
-const container = document.querySelector(".container");
+const input = document.querySelector(".placeholder");
+let btn = document.querySelector(".button");
+let newDictionary = document.querySelector(".dictionary");
 
-btn.addEventListener("click", addNewWord);
-input.addEventListener("keydown", (event) => {
-  if (event.key == "Enter") {
-    addNewWord();
-  }
+btn.addEventListener("click", () => {
+  const indexes = document.querySelectorAll(".index");
+
+  const divDict = document.createElement("div");
+  divDict.className = "row";
+  const div = document.querySelector(".russian");
+  const newElement = document.createElement("div");
+  const newIndex = document.createElement("span");
+  newElement.className = "russian";
+  newIndex.className = "index";
+  newElement.innerText = input.value;
+  newIndex.innerText = indexes.length + 1;
+  newElement.prepend(newIndex);
+  div.appendChild(newElement);
+
+  divDict.appendChild(newElement);
+  newDictionary.appendChild(divDict);
+
+  // input.value = '';
 });
 
-function addNewWord() {
-  const indexes = document.querySelectorAll(".index"); // обратились к о всем индексам и смотрим сколько их всего, что бы добавлять по порядку сл-й.  узнаем по длинне массива индексов
+const indexes = document.querySelectorAll(".index");
+indexes.forEach((el, index) => {
+  el[index].innerText = index;
+});
 
-  const newValue = document.createElement("div");
-  const newIndex = document.createElement("span"); // дальше нужно задать классы див и спан
-  newValue.className = "russian";
-  newIndex.className = "index";
-  newValue.innerText = input.value;
-  newIndex.innerText = indexes.length + 1; // смотрим на длину списка индексов и вставляем след-им
-  newValue.prepend(newIndex); // prepend ставит в начало списка
-  container.appendChild(newValue);
-  input.value = " ";
-}
+// btn.addEventListener("click", addNewWord);
+// input.addEventListener("keydown", (event) => {
+//   if (event.key == "Enter") {
+//     addNewWord();
+//   }
+// });
 
-// делаем удаление
-btn.addEventListener("click", ("src="./icons/krestik.svg"") {
-  for (i = 0; i < indexes.length; i++)
-  {
-    indexes[i].innerText = i + 1
-  }
-})
+// function addNewWord() {
+//   const indexes = document.querySelectorAll(".index"); // обратились к о всем индексам и смотрим сколько их всего, что бы добавлять по порядку сл-й.  узнаем по длинне массива индексов
+
+// // делаем удаление
+// btn.addEventListener("click", ("src="./icons/krestik.svg"") {
+//   for (i = 0; i < indexes.length; i++)
+//   {
+//     indexes[i].innerText = i + 1
+//   }
+// })
